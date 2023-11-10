eedf992e2f8265f45d20748bdf655df4070779c4
giripp <giripputra@gmail.com>
2023-11-10 14:47:40 +0700

    test add cors

--- frontend/app/router.options.ts
+++ frontend/app/router.options.ts
@@ -10,12 +10,14 @@ export default <RouterConfig> {
       path: '/',
       component: () => import('~/pages/index.vue').then(r => r.default || r),
       props: true,
+      cors: true
     },    
     {
       name: 'admin',
       path: '/admin',
       component: () => import('~/pages/admin.vue').then(r => r.default || r),
-      props: true
+      props: true,
+      cors: true
     },
     {
       name: 'enter',
@@ -25,93 +27,111 @@ export default <RouterConfig> {
     {
       name: 'enterm',
       path: '/enterm',
-      component: () => import('~/pages/enterm.vue')
+      component: () => import('~/pages/enterm.vue'),
+      cors: true
     },
     {
       name: 'register',
       path: '/register',
-      component: () => import('~/pages/register.vue')
+      component: () => import('~/pages/register.vue'),
+      cors: true
     },
     {
       name: 'tentang',
       path: '/tentang',
-      component: () => import('~/pages/about.vue').then(r => r.default || r)
+      component: () => import('~/pages/about.vue').then(r => r.default || r),
+      cors: true
     },
     {
       name: 'new',
       path: '/new',
-      component: () => import('~/pages/new.vue').then(r => r.default || r)
+      component: () => import('~/pages/new.vue').then(r => r.default || r),
+      cors: true
     },
     {
       name: 'notifications',
       path: '/notifications',
-      component: () => import('~/pages/notifications.vue')
+      component: () => import('~/pages/notifications.vue'),
+      cors: true
     },
     {
       name: 'settings',
       path: '/settings',
-      component: () => import('~/pages/settings.vue')
+      component: () => import('~/pages/settings.vue'),
+      cors: true
     },
     {
       name: 'signout',
       path: '/signout',
-      component: () => import('~/pages/signout.vue')
+      component: () => import('~/pages/signout.vue'),
+      cors: true
     },
     {
       name: 'dashboard',
       path: '/dashboard',
-      component: () => import('~/pages/dashboard.vue')
+      component: () => import('~/pages/dashboard.vue'),
+      cors: true
     },
     {
       name: 'gallery',
       path: '/gallery',
-      component: () => import('/pages/gallery.vue')
+      component: () => import('/pages/gallery.vue'),
+      cors: true
     },    
     {
       name: 'subgallery',
       path: '/gallery/:test',
-      component: () => import('/pages/gallery.vue')
+      component: () => import('/pages/gallery.vue'),
+      cors: true
     },
     {
       name: 'settings',
       path: '/settings',
-      component: () => import('/pages/settings.vue')
+      component: () => import('/pages/settings.vue'),
+      cors: true
     },
     {
       name: 'onboard',
       path: '/onboard',
-      component: () => import('/pages/onboard.vue')
+      component: () => import('/pages/onboard.vue'),
+      cors: true
     },
     {
       name: 'profile',
       path: '/@:username',
       component: () => import('~/pages/profile.vue').then(r => r.default || r),
-      props: true
+      props: true,
+      cors: true
     },
     {
       name: 'post',
       path: '/@:username/:postid',
       component: () => import('~/pages/post.vue').then(r => r.default || r),
-      props: true
+      props: true,
+      cors: true
     },
     {
       name: 'editpost',
       path: '/@:username/:postid/edit',
-      component: () => import('~/pages/new.vue')
+      component: () => import('~/pages/new.vue'),
+      cors: true
     },
     {
       name: 'page',
       path: '/p/:pageid',
-      component: () => import('~/pages/p.vue')
+      component: () => import('~/pages/p.vue'),
+      cors: true
     },
     {
       path: "/:pathMatch(.*)*",
-      component: () => import('~/pages/404.vue')
+      component: () => import('~/pages/404.vue'),
+      cors: true
     },
     {
       name: 'comment',
       path: '/comment/:commentid',
-      component: () => import('~/pages/comment.vue')
+      component: () => import('~/pages/comment.vue'),
+      cors: true
     }
   ].map(route => withMeta(_routes, route)),
 }
